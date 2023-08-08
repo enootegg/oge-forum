e1f1f8b2cc6e06093ededcb0a206b03f222e61cb
enootegg <vitaliybodnarchuk2002@gmail.com>
2023-08-08 22:45:11 +0300

    added schedule, info and prices

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,3 @@
-// Smooth scrolling when clicking an anchor link
-
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
-
 // Disable scroll when burger menu is open
 
 let checkbox = document.querySelector('input[name="burger"]');
