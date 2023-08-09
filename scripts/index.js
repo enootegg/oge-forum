49a3b5420fe50d8280b8b2471e7ee94d681aa8f3
enootegg <vitaliybodnarchuk2002@gmail.com>
2023-08-09 12:50:59 +0300

    fixed header and speakers

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,32 @@
 
 let checkbox = document.querySelector('input[name="burger"]');
 
-checkbox.addEventListener("change", () => {
+checkbox.addEventListener("change", function () {
+  console.log("aaa");
   document.querySelector("body").style.overflow = this.checked
     ? "hidden"
     : "inherit";
+});
+
+// Header scroll
+
+let className = "dark";
+let scrollTrigger = 100;
+
+window.onscroll = function () {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementById("mainHeader").classList.add(className);
+  } else {
+    document.getElementById("mainHeader").classList.remove(className);
+  }
+};
+
+// Close menu when we click on the link
+
+document.querySelectorAll(".menu-ul a").forEach((el) => {
+  el.addEventListener("click", () => {
+    checkbox.checked = false;
+    document.querySelector("body").style.overflow = "inherit";
+  });
 });
